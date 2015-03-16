afc1ca5e368563c99fa3318afbb36ff82e4b7e8e
Mark Feaver <mark.feaver@xero.com>
2015-03-16 20:14:48 +1300

    Finished Currently Playing basic view

---
+++ SpotifyTest/CurrentlyPlayingViewController.h
@@ -0,0 +1,13 @@
+//
+//  CurrentlyPlayingViewController.h
+//  SpotifyTest
+//
+//  Created by Mark Feaver on 16/03/15.
+//  Copyright (c) 2015 Mark Feaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CurrentlyPlayingViewController : UIViewController
+
+@end
---
+++ SpotifyTest/CurrentlyPlayingViewController.m
@@ -0,0 +1,26 @@
+//
+//  CurrentlyPlayingViewController.m
+//  SpotifyTest
+//
+//  Created by Mark Feaver on 16/03/15.
+//  Copyright (c) 2015 Mark Feaver. All rights reserved.
+//
+
+#import "CurrentlyPlayingViewController.h"
+
+@interface CurrentlyPlayingViewController ()
+@end
+
+@implementation CurrentlyPlayingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
